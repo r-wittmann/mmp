13eba71de3b599e0a42b09a2eb8e73b1bee28ccf
Rebecca <beccy.linke@gmail.com>
2016-07-11 15:34:58 +0200

    level

--- Spiderdungeon/Classes/GameScene.h
+++ Spiderdungeon/Classes/GameScene.h
@@ -28,10 +28,13 @@ public:
     private: float distance;
     private: float angle;
 public:
-	cocos2d::Sprite* _ball;
+	//cocos2d::Sprite* _ball;
 	cocos2d::Map<int, cocos2d::Sprite*> _bubbles;
 	std::map<int, cocos2d::Vector<cocos2d::Sprite*> > _linesPerBubble;
 	int level;
+	cocos2d::Sprite* _movingSpider;
+	cocos2d::Sprite* _movingSpiderLine;
+
        
     // a selector callback
     void pauseGame(cocos2d::Ref* sender);
@@ -40,7 +43,9 @@ public:
 	void removeCertainElement(cocos2d::Ref* sender, int bubble_hit);
 	void winLevel(cocos2d::Ref* sender);
 	void dumpSpider(cocos2d::Ref * sender, cocos2d::Sprite * spiderLine);
-    
+	void addSpiderLineAgain(float dt);
+	void test(Ref* sender);
+	
     void mouseClicked(cocos2d::Event* event, cocos2d::Sprite* canonStick, cocos2d::Sprite* canonBody);
     void mouseDragged(cocos2d::Event* event, cocos2d::Sprite* canonStick, cocos2d::Sprite* canonBody);
     void mouseReleased(cocos2d::Event* event, cocos2d::Sprite* canonStick, cocos2d::Sprite* canonBody);
